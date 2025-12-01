07f9eec328b43ee657d41b705267cf5c5179eac7
Hyungmin Oh <hyungoh@umich.edu>
2025-11-30 21:25:51 -0500

    capitalization updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder, {
   action as createOrderAction,
-} from './features/order/createOrder';
+} from './features/order/CreateOrder';
 import Order, { loader as orderLoader } from './features/order/Order';
 import { action as UpdateOrderAction } from './features/order/UpdateOrder';
 
